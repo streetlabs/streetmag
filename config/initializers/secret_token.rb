afd19c341c2c3db879a5b45388daddc3b2b45b0d
Mark Madsen <mark@agileanimal.com>
2010-11-27 19:36:26 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Streetmag::Application.config.secret_token = '00e6be1d5dc421f4b52a05dc802684ba1456840f8a3aafab582b59c810d8429b262f8c6962882871803adbb1f22326f4df662b3cb83a1b8314d3996d23f9d3a2'
