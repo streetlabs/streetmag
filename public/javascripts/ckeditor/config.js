8410dd0be06f192620052819278c4a2fa61164a2
Mark Madsen <mark@agileanimal.com>
2011-03-03 00:47:24 -0700

    Changes to the toolbar in cKeditor

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -7,28 +7,25 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 CKEDITOR.addStylesSet( 'streetmag_styles',
 [
     // Block Styles
-	
     { name : 'Regular' , element : 'p' },
-    { name : 'Title' , element : 'div', attributes : { 'class' : 'title' } },
-    { name : 'Author' , element : 'div', attributes : { 'class' : 'author' } },
-    { name : 'Affiliation' , element : 'div', attributes : { 'class' : 'affiliation' } },
-    { name : 'Abstract' , element : 'div', attributes : { 'class' : 'abstract' } },
-    { name : 'Heading' , element : 'h3', attributes : { 'class' : 'heading' } },
-    { name : 'Subheading' , element : 'h4', attributes : { 'class' : 'subheading' } },
+    { name : 'Title' , element : 'h1', attributes : { 'class' : 'title' } },
+    { name : 'Author' , element : 'h2', attributes : { 'class' : 'author' } },
+    { name : 'Abstract' , element : 'p', attributes : { 'class' : 'abstract' } },
+    { name : 'Heading' , element : 'h2', attributes : { 'class' : 'heading' } },
+    { name : 'Subheading' , element : 'h3', attributes : { 'class' : 'subheading' } },
     { name : 'Epigraph' , element : 'p', attributes : { 'class' : 'epigraph' } },
 
 	
-    { name : 'Caption' , element : 'div', attributes : { 'class' : 'caption' } },
-    { name : 'Stanza' , element : 'div', attributes : { 'class' : 'stanza' } },
-    { name : 'Note' , element : 'div', attributes : { 'class' : 'note' } },
-    { name : 'Definition' , element : 'div', attributes : { 'class' : 'definition' } },
-
-	
-    { name : 'Endnotes' , element : 'div', attributes : { 'class' : 'endnotes' } },
-    { name : 'Citation' , element : 'div', attributes : { 'class' : 'citation' } },
-    { name : 'Bio' , element : 'div', attributes : { 'class' : 'bio' } },
+    { name : 'Caption' , element : 'p', attributes : { 'class' : 'caption' } },
+    { name : 'Stanza' , element : 'p', attributes : { 'class' : 'stanza' } },
+    { name : 'Note' , element : 'p', attributes : { 'class' : 'note' } },
+    { name : 'Endnotes' , element : 'p', attributes : { 'class' : 'endnotes' } },
+    { name : 'Citation' , element : 'p', attributes : { 'class' : 'citation' } },
+    { name : 'Bio' , element : 'p', attributes : { 'class' : 'bio' } },
 
     // Inline Styles
+    { name : 'Affiliation' , element : 'span', attributes : { 'class' : 'affiliation' } },
+    { name : 'Definition' , element : 'span', attributes : { 'class' : 'definition' } },
     { name : 'Important', element : 'span', attributes : { 'class' : 'important' } },
     { name : 'Emphasize', element : 'span', attributes : { 'class' : 'emphasize' } },
     { name : 'Highlight: Yellow', element : 'span', styles : { 'background-color' : 'Yellow' } }
@@ -71,22 +68,28 @@ CKEDITOR.editorConfig = function( config )
         ['Image','Embed','Flash','Attachment','Table','HorizontalRule','Smiley','SpecialChar','PageBreak']
     ];
 
+	config.toolbar = 'Admin';
+
+//	config.toolbar_Admin =
+	config.toolbar_Article =
+	
+	[
+    	['Styles','Format','-','Link','Unlink','Anchor','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','Find','Replace','-','PasteText','PasteFromWord'],
+		['RemoveFormat','-','Bold','Italic','Strike','-','Subscript','Superscript','SpecialChar'],
+	    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+	    ['Table','-','ShowBlocks','-','Source','-','Maximize'],
+	];
+	
 	config.toolbar = 'Article';
 
-	config.toolbar_Article =
+//	config.toolbar_Article =
+	config.toolbar_Admin =
 	[
-	
-    	['Styles','Format'],
-		['RemoveFormat'],
-	    ['Link','Unlink','Anchor'],
-		['Find','Replace'],
-	    ['PasteText','PasteFromWord'],
-	    ['ShowBlocks','Maximize'],
-	    '/',
-	    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-	    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+    	['Styles','Format','-','Link','Unlink','Anchor','-','Find','Replace','-','PasteText','PasteFromWord','-','ShowBlocks'],
+		['RemoveFormat','-','Bold','Italic','Strike','-','Subscript','Superscript','SpecialChar'],
+	    ['NumberedList','BulletedList','-','Blockquote'],
 	    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-	    ['Table','SpecialChar']
+	    ['Table','-','Maximize'],
 	];
 	
 	config.stylesCombo_stylesSet = 'streetmag_styles';
