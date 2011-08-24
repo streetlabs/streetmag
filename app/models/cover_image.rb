2accc98413d3828ca73ee2211e4b6e257f6df874
Mark Madsen <mark@agileanimal.com>
2011-08-23 22:51:55 -0600

    working on ckeditor JS issue:

--- app/models/cover_image.rb
+++ app/models/cover_image.rb
@@ -3,7 +3,7 @@ class CoverImage < ActiveRecord::Base
   
   # Paperclip
   has_attached_file :photo,
-                    :styles => { :large => "480x360<", :thumb => "100x100<" },
+                    :styles => { :large => "480", :thumb => "120" },
                     :storage => :cloud_files,
                     :path => "coverimages/:attachment/:id/:style/:basename.:extension",
                     :cloudfiles_credentials => "#{Rails.root}/config/rackspace_cloudfiles.yml"
