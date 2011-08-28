9e526bf53e7581cbfa660d61197c5d6063a559dd
Mark Madsen <mark@agileanimal.com>
2011-08-28 00:23:12 -0600

    fixed error where articles were being shared across issues when not in static sections

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,9 @@ class ArticlesController < ApplicationController
   def show
     @publication = Publication.find_by_name!(request.subdomain)
     @article = @publication.articles.find(params[:id])
+    @issue = @article.arrangement.issue
+    @articles = @article.section.is_static ? @article.section.articles.order("arrangements.position ASC") : @article.section.articles.where("arrangements.issue_id = ?", @issue.id).order("arrangements.position ASC")
+
   end
 
 end
